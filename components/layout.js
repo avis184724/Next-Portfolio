325252d8d3c4d10737a28b9abe2a04ceebf6c8e4
avis184724 <ckdtjqdld9632@naver.com>
2024-08-27 20:11:38 +0900

    8_27

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,9 @@
 import Header from "./header"
 import Footer from "./footer"
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
     return (
-        <>  
+        <>
             <Header></Header>
             <h1>레이아웃</h1>
             <div>{children}</div>
--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ export default function Home() {
     <Layout>
       <Head>
         <title>이재황의 포트폴리오</title>
-        <meta name="description" content="나의 포트폴리오" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="나의 포트폴리오"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <h1>이재황 포트폴리오</h1>
